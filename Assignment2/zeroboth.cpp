5df859295f495bf99af52a2c4d6446d0d0a993af
Jessica V <74572927+jeviner@users.noreply.github.com>
2021-01-25 17:46:07 -0800

    completed zeroboth.cpp

--- Assignment2/zeroboth.cpp
+++ Assignment2/zeroboth.cpp
@@ -11,7 +11,20 @@ zeroboth.cpp
 
 using namespace std;
 
+//call by reference paramters a and b to set referenced variables to 0
+void zeroBoth(int* a, int* b){
+  *a = 0;
+  *b = 0;
+}
+
+
 
 int main(){
+  int x = 10;
+  int y = 7;
+  int* xp = &x;
+  int* yp = &y;
+  zeroBoth(xp, yp);
+  cout << x << " " << y << endl;
   return 0;
 }
