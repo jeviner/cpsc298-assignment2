6c2c14ea2b2b78cac18879e70fe2c724ad6126f8
Jessica V <74572927+jeviner@users.noreply.github.com>
2021-01-25 17:32:34 -0800

    set up for files

---
+++ Assignment2/conversion.cpp
@@ -0,0 +1,19 @@
+/*
+Jessica Viner
+2345566
+viner@chapman.edu
+CPSC 298 - 07
+Programming Assignment 2: Loops, Arrays & Functions
+conversion.cpp
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+  
+  return 0;
+}
